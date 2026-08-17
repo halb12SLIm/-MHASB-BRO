daad1b3e71bea186a7c77c64282c582348db21e8
halb12SLIm <meassehalid@gmail.com>
2026-08-18 01:32:44 +0300

    feat: add Capacitor support for Android
    Initialize Capacitor project configuration and add GitHub workflow to support mobile app building.

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mhasb.pro',
+  appName: 'محاسب سليم برو',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
